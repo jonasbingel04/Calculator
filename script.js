bd2eebc55c3ac58a4ea83e799ca4f001f60585b1
Jonas Bingel <jonas@bingel.eu>
2025-12-04 00:01:13 +0100

    added operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let number1;
+let number2;
+let operator;
+
 function add(x, y){
     return x + y;
 }
@@ -9,4 +13,21 @@ function multiply(x,y){
 }
 function divide(x, y){
     return x / y
+}
+function operate(){
+    if(operator == "+"){
+        return add(number1, number2);
+    }
+    else if(operater == "-"){
+        return subtract(number1,number2);
+    }
+    else if(operator == "*"){
+        return multiply(number1,number2);
+    }
+    else if(operator == "/"){
+        return divide(number1,number2);
+    }
+    else{
+        console.log("Error in funtion operate")
+    }
 }
